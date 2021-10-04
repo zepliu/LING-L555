5597e056b1e9cbe98c2ae1db35e76e9363e87ea8
Zeping <liuzeping@liuzepingdeMacBook-Pro.local>
2021-10-04 14:17:12 -0400

    Tokenization

--- tokeniser.py
+++ tokeniser.py
@@ -8,7 +8,7 @@ for line in lines:
 	n = n + 1
 	print("\n# sent_id = ", n)
 	sent = line.strip()
-	print("# text = ", sent, "\n")
+	print("# text = ", sent)
 	word_id = 1
 	sent = sent.replace("," , " , ")
 	sent = sent.replace("." , " . ")
