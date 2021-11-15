12e9188172958d8d7b94db7d93e0dfa6cc61d4bf
Zeping Liu <zeping@Zepings-MacBook-Pro.local>
2021-11-15 14:05:58 -0500

    Writing the tagger

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import sys
 
-pos = []   # Create a list to store form and tag
+uniqList = []   # To compute count in the second column
+posDict = {}    # The frequency of tags 
+pairDict = {}   # The frequency of [form,tag]
+formDict = {}   # The frequency of tokens on condition of being put into the first colomn (1/frequency)
 
 base = open('wiki.conllu', 'r')
 
@@ -11,16 +14,28 @@ for line in base.readlines():
     row = line.split('\t')
     form = row[1]
     tag = row[3]
-    pos.append((form,tag))       # Put form and tag together
+    uniqList.append((form,tag))
 
-uniqList = {}    # Create a dict to store count data
+for item in uniqList:
+    if item not in posDict:
+        pairDict[item] = 0
+    pairDict[item] = pairDict[item] + 1
 
-for item in pos:
-    if item not in uniqList:
-        uniqList[item] = 0
-    uniqList[item] = uniqList[item] + 1
+counter = 0
+for (form,tag) in uniqList:
+    if tag not in posDict:
+        posDict[tag] = 0
+    posDict[tag] = posDict[tag] + 1
+    if form not in formDict:
+        formDict[form] = 0
+    formDict[form] = formDict[form] + 1
 
 print("{}\t{}\t{}\t{}\n".format("# P","count", "tag","form"))  # Create headings
 
-for key, value in uniqList.items():
-    print("{:.2f}\t{}\t{}\t{}".format(value/sum(uniqList.values()), value,key[1],key[0]))
+for key, value in posDict.items():
+    print("{:.2f}\t{}\t{}\t{}".format(value/sum(posDict.values()), value, key, '_'))
+
+for key, value in pairDict.items():
+    form = key[0]
+    tokenFreq = formDict[form]      
+    print("{:.2f}\t{}\t{}\t{}".format(value/tokenFreq, value , key[1], key[0]))
