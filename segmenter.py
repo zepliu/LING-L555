76a01bdbdfa7f3f6587fc8576da4194b1fa5e12e
Zeping Liu <zeping@Zepings-MacBook-Pro.local>
2021-12-08 14:27:03 -0500

    Final Project

--- segmenter.py
+++ segmenter.py
@@ -3,6 +3,7 @@ import sys
 line = sys.stdin.readline()
 
 while line:
-	line = line.replace('. ', '.' + '\n')
+	line = line.replace('。', '。' + '\n')
+    line = line.replace('！', '!' + '\n')
 	print(line)
 	line = sys.stdin.readline()
